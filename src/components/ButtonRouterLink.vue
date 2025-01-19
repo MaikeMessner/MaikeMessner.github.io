a46680b98988b1704938caea9dc9d602dc55bdca
Henning Janßen <development@henning-janssen.net>
2025-01-19 17:46:33 +0100

    comp: RouteLink that uses button instead of a

---
+++ src/components/ButtonRouterLink.vue
@@ -0,0 +1,32 @@
+<script>
+  import { RouterLink } from "vue-router";
+
+  export default {
+    name: "ButtonRouterLink",
+    inheritAttrs: false,
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    props: {
+      // add @ts-ignore if using TypeScript
+      ...RouterLink.props,
+      inactiveClass: String(""),
+    },
+  };
+</script>
+
+<template>
+  <RouterLink
+    v-slot="{ isActive, href, navigate }"
+    v-bind="$props"
+    custom
+  >
+    <button
+      v-bind="$attrs"
+      :href="href"
+      :class="isActive ? activeClass : inactiveClass"
+      @click="navigate"
+    >
+      <slot />
+    </button>
+  </RouterLink>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ import vMasonry from "./directives/masonry";
 import vHtmlSlot from "./directives/htmlSlot";
 import { setTitle } from "./state/title";
 import { router } from "./router";
+import ButtonRouterLink from "./components/ButtonRouterLink.vue";
 
 // eslint-disable-next-line import/no-named-as-default-member
 const i18NextInitialized = i18next
@@ -55,6 +56,7 @@ i18NextInitialized
       .use(I18NextVue, { i18next })
       .directive("masonry", vMasonry)
       .directive("html-slot", vHtmlSlot)
+      .component("ButtonRouterLink", ButtonRouterLink)
       .mount("#app");
   })
   .catch((err) => console.error(err));
