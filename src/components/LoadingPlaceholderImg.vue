d6f308db761e2ba6a46f01798d7feb7014cc5e57
Henning Janßen <development@henning-janssen.net>
2025-01-20 21:29:11 +0100

    feat: prepare img loading with placeholder img

---
+++ src/components/LoadingPlaceholderImg.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { useImage } from "@vueuse/core";
+
+  export default {
+    name: "LoadingPlaceholderImg",
+    inheritAttrs: false,
+
+    props: {
+      src: String(""),
+      loadingSrc: String(""),
+    },
+
+    data() {
+      return {
+        isLoading: false as boolean,
+      };
+    },
+
+    mounted() {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const { isLoading } = useImage({ src: this.isLoading });
+      this.isLoading = isLoading;
+    },
+  };
+</script>
+
+<template>
+  <img
+    :src="isLoading ? loadingSrc || src : src"
+    v-bind="$attrs"
+  />
+</template>
